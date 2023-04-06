04f15bfb8f4f21272c0f1c6e63108d6972ef2a7a
Your Name~ <dpino131@gmail.com>
2023-04-06 02:46:38 -0500

    Fifth-Class

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,113 +1,87 @@
 import pygame
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
-
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING, DEFAULT_TYPE, SHIELD_TYPE, RUNNING_SHIELD,JUMPING_SHIELD,DUCKING_SHIELD
+RUN_IMG = {DEFAULT_TYPE:RUNNING,SHIELD_TYPE:RUNNING_SHIELD }
+JUMP_IMG = {DEFAULT_TYPE:JUMPING,SHIELD_TYPE:JUMPING_SHIELD}
+DUCK_IMG = {DEFAULT_TYPE:DUCKING,SHIELD_TYPE:DUCKING_SHIELD}
 class Dinosaur(Sprite):
-    X_POS = 80
-    Y_POS = 310
-    JUMP_SPEED = 8.5
-    DUCK_TIME = 20 # tiempo que el dinosaurio permanece agachado
-
-    def __init__(self):
-        self.image = RUNNING[0]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x =  self.X_POS
-        self.dino_rect.y = self.Y_POS
-        self.step_index = 0 #que pierna toca
-        self.dino_run = True #dice si esta corriendo
-        self.dino_jump = False #dice si esta saltando
-        self.jump_speed = self.JUMP_SPEED
-        self.is_ducked = False #dice si esta agachado
-        self.duck_time = 0 # Inicializa el tiempo que el dinosaurio permanece agachado en 0
-        
+  
+  X_POS = 80
+  Y_POS = 310
+  JUMP_SPEED = 8.5
+  Y_POS_DUCK = 340
+  
+  def __init__(self):
+    self.type = DEFAULT_TYPE
+    self.image = RUN_IMG[self.type][0]
+    self.dino_rect = self.image.get_rect()
+    self.dino_rect.x = self.X_POS
+    self.dino_rect.y = self.Y_POS
+    self.step_index = 0
+    self.dino_run = True
+    self.dino_jump = False
+    self.dino_duck = False
+    self.jump_speed = self.JUMP_SPEED
+    self.has_power_up = False
+    self.power_time = 0
     
-
-    def update(self,user_input):
-        if self.dino_run:
-            self.run()
-        elif self.dino_jump:
-            self.jump()
-
-        if user_input[pygame.K_UP] and not self.dino_jump:
-            self.dino_run = False
-            self.dino_jump = True
-        elif not self.dino_jump:
-            self.dino_run = True
-            self.dino_jump = False
-
-        #if self.is_ducked: # Si el dinosaurio está agachado
-        #    self.duck_time += 1 # Incrementa el tiempo que el dinosaurio ha permanecido agachado
-        #    if self.duck_time > self.DUCK_TIME: # Si el tiempo ha pasado
-        #        self.is_ducked = False # Establece la bandera de agachado como falsa
-        #        self.duck_time = 0 # Reinicia el tiempo que el dinosaurio permanece agachado
-        #elif user_input[pygame.K_DOWN] and not self.dino_jump: # Si el usuario pulsa la tecla de agacharse y el dinosaurio no está saltando ni agachado
-        #    self.duck()
-        #elif not self.dino_jump:
-        #    self.run()
-
-        
-        # Comprueba si se ha presionado la tecla hacia abajo y si el dinosaurio no está saltando ni agachado
-        if user_input[pygame.K_DOWN] and not self.dino_jump and not self.is_ducked:
-            self.duck()
-        # Comprueba si la tecla hacia abajo ya no se está presionando y si el dinosaurio está agachado
-        elif not user_input[pygame.K_DOWN] and self.is_ducked:
-            self.is_ducked = False
-            self.run()
-        
-        # Si el dinosaurio está agachado, aumenta el tiempo que ha estado agachado y comprueba si ya ha pasado el tiempo máximo
-        if self.is_ducked:
-            self.duck_time += 1
-            if self.duck_time > self.DUCK_TIME:
-                self.is_ducked = False
-                #self.duck_time = 0
-
-        if self.step_index >= 10:
-            self.step_index = 0
-
-
+  def update(self, user_input):
+    if self.dino_run:
+      self.run()
+    elif self.dino_jump:
+      self.jump()
+    elif self.dino_duck:
+      self.duck()   
+      
+    if user_input[pygame.K_UP] and not self.dino_jump:
+      self.dino_jump = True
+      self.dino_run = False
+    elif user_input[pygame.K_DOWN] and not self.dino_jump:
+      self.dino_jump = False
+      self.dino_run = False
+      self.dino_duck = True
+    elif not self.dino_jump:
+      self.dino_duck = False
+      self.dino_run = True
     
-
-    def run(self):
-        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
-        self.step_index += 1
-        
-
-    def jump(self):
-        self.image = JUMPING
-        self.dino_rect.y = self.jump_speed *4
-        self.jump_speed -= 0.8
-
-        if self.jump_speed < - self.JUMP_SPEED:
-            self.dino_rect.y = self.Y_POS
-            self.dino_jump = False
-            self.jump_speed = self.JUMP_SPEED
-
-
-
-
-    def duck(self):
-        self.image = DUCKING[0]  # Cambia la imagen del dinosaurio a la imagen de agacharse
-        self.dino_rect = self.image.get_rect()  # Ajusta la posición
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS + 40 #más 40 unidades para simular el efecto de que el dinosaurio se agacha
-        self.is_ducked = True  # Establece la bandera de agachado como verdadera
-
-    def draw(self,screen):
-        screen.blit(self.image,(self.dino_rect.x,self.dino_rect.y))
-
-    def reset_dinosaur(self):
-        self.image = RUNNING[0]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x =  self.X_POS
-        self.dino_rect.y = self.Y_POS
-        self.step_index = 0 #que pierna toca
-        self.dino_run = True #dice si esta corriendo
-        self.dino_jump = False #dice si esta saltando
-        self.jump_speed = self.JUMP_SPEED
-        self.is_ducked = False #dice si esta agachado
-        self.duck_time = 0 
-
+    if self.step_index >= 10:
+      self.step_index = 0
+  
+  def run(self):
+    self.image = RUN_IMG[self.type][self.step_index // 5]
+    self.dino_rect = self.image.get_rect()
+    self.dino_rect.x = self.X_POS
+    self.dino_rect.y = self.Y_POS
+    self.step_index += 1
+  
+  def jump(self):
+    self.image = JUMP_IMG[self.type]
+    self.dino_rect.y -= self.jump_speed * 4
+    self.jump_speed -= 0.8
+    
+    if self.jump_speed < -self.JUMP_SPEED:
+      self.dino_rect.y = self.Y_POS
+      self.dino_jump = False
+      self.jump_speed = self.JUMP_SPEED
+  
+  def duck(self):
+    self.image = DUCK_IMG[self.type][0][self.step_index// 5]
+    self.dino_rect = self.image.get_rect()
+    self.dino_rect.x = self.X_POS
+    self.dino_rect.y = self.Y_POS_DUCK
+    self.step_index += 1
+  
+  def draw(self, screen):
+    screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+    
+  def reset(self):
+    self.image = RUNNING[0]
+    self.dino_rect = self.image.get_rect()
+    self.dino_rect.x = self.X_POS
+    self.dino_rect.y = self.Y_POS
+    self.step_index = 0
+    self.dino_run = True
+    self.dino_jump = False
+    self.dino_duck = False
+    self.jump_speed = self.JUMP_SPEED
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,14 +1,14 @@
-from turtle import _Screen
 import pygame
-import os
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS,FONT_STYLE
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS ,DEFAULT_TYPE
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 from dino_runner.components.menu import Menu
+from dino_runner.components.counter import Counter
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
+
 class Game:
     GAME_SPEED = 20
-    HIGHSCORE_FILE = "highscore.txt"
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
@@ -21,34 +21,28 @@ class Game:
         self.y_pos_bg = 380
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
-        self.menu = Menu("Press and key to start..." ,self.screen)
+        self.menu = Menu(self.screen)
         self.running = False
-        self.death_count = 0
-        self.score = 0
-        self.highscore = self.load_highscore()
-        self.total_deaths = 0 #contar las muertes
-    #si se dediene por algun motivo se cierre
+        self.score = Counter()
+        self.death_count = Counter()
+        self.highest_score = Counter()
+        self.power_ups = PowerUpManager
     def execute(self):
         self.running = True
         while self.running:
-            if not self.playing: #error en linea 27
+            if not self.playing:
                 self.show_menu()
         pygame.display.quit()
         pygame.quit()
 
-        
     def run(self):
-        self.obstacle_manager.reset_obstacle()
-        self.player.reset_dinosaur()
-        self.score = 0 
-        self.game_speed = self.GAME_SPEED
+        self.reset_game()
         # Game loop: events - update - draw
         self.playing = True
         while self.playing:
             self.events()
             self.update()
             self.draw()
-        #pygame.quit()
 
     def events(self):
         for event in pygame.event.get():
@@ -59,9 +53,9 @@ class Game:
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self)
-        self.update_score()
-        
-        
+        self.power_up_manager.update(self)
+        self.score.update()
+        self.update_game_speed()
 
     def draw(self):
         self.clock.tick(FPS)
@@ -69,11 +63,10 @@ class Game:
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
-        self.draw_score() #dibuja el cuadro del conteo en pantalla
+        self.power_up_manager.draw(self.screen)
+        self.score.draw(self.screen)
         pygame.display.update()
-        
-        
-        #pygame.display.flip()
+        # pygame.display.flip()
 
     def draw_background(self):
         image_width = BG.get_width()
@@ -83,72 +76,45 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
-
+        
     def show_menu(self):
+        self.menu.reset_screen_color(self.screen)
         half_screen_height = SCREEN_HEIGHT // 2
         half_screen_width = SCREEN_WIDTH // 2
-        self.menu.reset_screen_color(self.screen)
-
-        if self.death_count == 0: #si el contador de muertes es igual a 0, 
-            #el método dibuja el menú en la pantalla. Si el contador de muertes 
-            #es mayor que 0, el método actualiza el mensaje del menú para 
-            # mostrar la puntuación actual del jugador 
-            self.menu.draw(self.screen)
-        else:
-            #self.menu.update_message(f"Your score: {self.score} +\n Highscore: {self.highscore}")#antes estava new message
-            self.menu.update_message(f"Your score: {self.score} \n Highscore: {self.highscore}")
-            #self.menu.update_message(f"Your score: {self.score}")#muestra la puntuacion
-            self.draw_score()
-
-            self.menu.draw(self.screen)
-            self.save_highscore()
-            self.total_deaths += 1
-        self.screen.blit(ICON,(half_screen_width-50,half_screen_height-140))
-        #muestra una imagen del dinosaurio
-        self.menu.update(self)
-    #guarda el puntaje mas alto
-
-    def save_highscore(self):
-        # Si la puntuación actual del jugador es mayor que la puntuación máxima anterior
-        if self.score > self.highscore:
-            # Se abre el archivo "highscore.txt" en modo escritura
-            with open(self.HIGHSCORE_FILE, "w") as f:
-                # Se escribe la puntuación actual en el archivo
-                f.write(str(self.score))
-            # Se actualiza la puntuación máxima con la puntuación actual
-            self.highscore = self.score
-
-    def load_highscore(self):
-        # Revisa si existe el archivo de highscore
-        if os.path.exists(self.HIGHSCORE_FILE):
-            with open(self.HIGHSCORE_FILE, "r") as f:
-                # Carga la puntuación máxima anterior guardada en el archivo
-                highscore = int(f.read())
-        else:
-            # Si el archivo no existe, se deja la puntuación en 0
-            highscore = 0
-        return highscore  # Retorna la puntuación máxima
-
-    def update_score(self):#aumenta el valor de score
-        self.score += 1
-        if self.score % 100 == 0 and self.game_speed < 500: #aumenta la velocida
-            self.game_speed +=5
         
-    def draw_score(self):
-        font = pygame.font.Font(FONT_STYLE, 30)
-        text = font.render(f'Score: {self.score}',True,(0,0,0))
-        #muestra el hig score en pantalla puntuacion maxima
-        highscore_text = font.render(f'Highscore: {self.highscore}', True, (0, 0, 0))
-        total_deaths_text = font.render(f'Total deaths: {self.total_deaths}', True, (0, 0, 0))
-        text_rect = text.get_rect()
-        highscore_rect = highscore_text.get_rect()
-        total_deaths_rect = total_deaths_text.get_rect()
-        text_rect.center = (1000, 50)
-        #score_rect.center = (1000, 50)
-        highscore_rect.center = (1000, 80)
-        total_deaths_rect.center = (1000, 110)
-        self.screen.blit(text, text_rect)
-        #self.screen.blit(score_text, score_rect)
-        self.screen.blit(highscore_text, highscore_rect)
-        self.screen.blit(total_deaths_text, total_deaths_rect)
+        if self.death_count.count == 0:
+            self.menu.draw(self.screen, 'Press any key to start ...')
+        else:
+            self.update_highest_score()
+            self.menu.draw(self.screen, 'Game over. Press any key to restart')
+            self.menu.draw(self.screen, f'Your score: {self.score.count}', half_screen_width, 350, )
+            self.menu.draw(self.screen, f'Highest score: {self.highest_score.count}', half_screen_width, 400, )
+            self.menu.draw(self.screen, f'Total deaths: {self.death_count.count}', half_screen_width, 450, )
+        
+        self.screen.blit(ICON, (half_screen_width - 50, half_screen_height - 140))
+        
+        self.menu.update(self)
+        
+    def update_game_speed(self):
+        if self.score.count % 100 == 0 and self.game_speed < 500:
+            self.game_speed += 5
+            
+    def update_highest_score(self):
+        if self.score.count > self.highest_score.count:
+            self.highest_score.set_count(self.score.count)
+            
+    def reset_game(self):
+        self.obstacle_manager.reset_obstacles()
+        self.score.reset()
+        self.game_speed = self.GAME_SPEED
+        self.player.reset()
+        self.power_up_manager.reset()
 
+    def draw_power_up_time(self):
+        if self.player.has_power_up:
+            time_to_show = ((self.player.power_time_up - pygame.time.get_ticks)/1000,2)
+            if time_to_show >= 0:
+                self.menu.draw(self.screen, '{self.player.power_time.capitalize()} enable for {time_to_show} seconds', 500,50)
+            else:
+                self.player.has_power_up = False
+                self.player.type = DEFAULT_TYPE
